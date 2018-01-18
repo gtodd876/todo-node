7f408e7fa64171e1e561797a4202b0dff590e3ef
Todd Matthews <gtmatthews@gmail.com>
2018-01-18 17:15:34 -0500

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -15,10 +15,21 @@ app.post('/todos', (req, res) => {
   });
   todo.save().then(
     doc => {
-      res.status(200).send(doc);
+      res.send(doc);
     },
     e => {
-      console.error(res.send(e));
+      res.status.apply(400).send(e);
+    }
+  );
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
     }
   );
 });
@@ -26,3 +37,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = { app };
